f7dd92e982979f5420fc514271cb8e4f27d744e1
MINIROC <dm4407721@gmail.com>
2025-08-13 23:39:56 +0200

    добавил новый урок

---
+++ lesson 11, YAML, JSON, CSV.py
@@ -0,0 +1,50 @@
+from random import sample
+
+import yaml
+#создание yaml файла
+#write
+l_connections=[
+    {
+        "user_name": "elt_user",
+        "password": "123",
+        'host': '127.0.0.1'
+    },
+    {
+        "user_name": "test",
+        "password": "456"
+    }
+]
+
+# создание самого файла
+with open(r'connections.yaml', 'w') as file:
+    documents = yaml.dump(l_connections, file)
+
+# reeding
+
+with open(r'connections.yaml') as file:
+    connections = yaml.load(file, Loader=yaml.FullLoader)
+    print(connections)
+
+# JSON
+import json
+
+dictionary={
+    'name': 'Oleg1',
+    'rollno': 56,
+    'cpga': 6.7,
+    'phonenumber':'8800555454'
+}
+
+#индент это вид форматирования, для красоты и удобста, можно и без него
+json_object = json.dumps(dictionary, indent=1)
+
+#writing
+with open('sample.json', 'w') as outfile:
+    outfile.write(json_object)
+
+#reading
+
+with open('sample.json', 'r') as openfile:
+    json_object = json.load(openfile)
+
+print(json_object)
